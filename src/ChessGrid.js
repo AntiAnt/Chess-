cc473d396076df720d6d8a9e5775877a5ff0a21a
Brett Jurman <i.be.brett@gmail.com>
2019-07-22 19:45:37 -0400

    why does htis happen?

--- src/ChessGrid.js
+++ src/ChessGrid.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './ChessGrid.css';
 
-export function ChessGrid() {
-  return (
-    <div className="App">
+import white_pawn from './white-pawn.png'
+
+export class ChessGrid extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      show_image: false
+    };
+  }
+
+  onClick() {
+    this.setState({ show_image: !this.state.show_image} );
+  }
+ 
+  render() {
+    return (
+      <>
+
+      <button onClick={()=>this.onClick()}>Show Image</button>
       <section id="container">
-        <div className="black"></div> 
-        <div className="white"></div> 
+        <div className="black">
+        </div> 
+        <div className="white">
+          {this.state.show_image && (<div><img width="64px" height="64px" className="piece" src={white_pawn} alt="piece"></img></div>)}
+        </div> 
         <div className="black"></div> 
         <div className="white"></div> 
         <div className="black"></div> 
@@ -77,8 +96,9 @@ export function ChessGrid() {
         <div className="white"></div> 
         <div className="black"></div> 
       </section>
-    </div>
-  );
+      </>
+    );
+  }
 }
 
 
